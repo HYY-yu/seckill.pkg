7f14cd60b2932d0bd350e10b43541985e54ebfaf
yufeng <yufeng@zonst.cn>
2022-09-22 11:50:37 +0800

    fix: err cause

--- pkg/mysqlerr_helper/err_helper.go
+++ pkg/mysqlerr_helper/err_helper.go
@@ -3,12 +3,14 @@ package mysqlerr_helper
 import (
 	"github.com/VividCortex/mysqlerr"
 	"github.com/go-sql-driver/mysql"
+	"github.com/gogf/gf/v2/errors/gerror"
 )
 
 // err helpers
 
 // IsMysqlDupEntryError Mysql返回唯一索引冲突
 func IsMysqlDupEntryError(err error) bool {
+	err = gerror.Cause(err)
 	if driverErr, ok := err.(*mysql.MySQLError); ok {
 		if driverErr.Number == mysqlerr.ER_DUP_ENTRY {
 			// 唯一索引错误
